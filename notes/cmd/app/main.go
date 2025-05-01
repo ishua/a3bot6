248d18785820a7bd64185d0c3ca09fb44853e366
amamyrin <amamyrin@ozon.ru>
2025-05-01 19:02:27 +0300

    add notes

---
+++ notes/cmd/app/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cristalhq/aconfig"
+	"github.com/cristalhq/aconfig/aconfigyaml"
+	"github.com/ishua/a3bot6/mcore/pkg/logger"
+)
+
+type MyConfig struct {
+	RepoPath        string `default:"data/fsnotes" usage:" path to repository fsnotes"`
+	RepoUrl         string `required:"true"`
+	RepoAccessToken string `env:"REPOACCESSTOKEN" required:"true"`
+	MCoreAddr       string `default:"http://127.0.0.1:8080" usage:"host and port for mcore"`
+	TBotSecret      string `default:"test" usage:"secret key for api"`
+	Debug           bool   `default:"false" usage:"turn on debug mode"`
+}
+
+var (
+	cfg MyConfig
+)
+
+func main() {
+
+	//config init
+	loader := aconfig.LoaderFor(&cfg, aconfig.Config{
+		Files: []string{"conf/mcore_config.yaml"},
+		FileDecoders: map[string]aconfig.FileDecoder{
+			".yaml": aconfigyaml.New(),
+		},
+	})
+	if err := loader.Load(); err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	if cfg.Debug {
+		logger.SetLogLevel(logger.DEBUG)
+		logger.Debugf("debug logging enabled")
+	}
+
+	fmt.Print("done")
+
+}
---
+++ notes/internal/clients/gitapi/client.go
@@ -0,0 +1,92 @@
+package gitapi
+
+import (
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"os"
+	"time"
+)
+
+type Repo struct {
+	*git.Repository
+	Token    string
+	Username string
+	Email    string
+}
+
+func NewClient(path string, url string, token string, username string, email string) (*Repo, error) {
+	r, err := git.PlainOpen(path)
+	if err != nil {
+		r, err = git.PlainClone(path, false, &git.CloneOptions{
+			Auth: &http.BasicAuth{
+				Username: username, // yes, this can be anything except an empty string
+				Password: token,
+			},
+			URL:      url,
+			Progress: os.Stdout,
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &Repo{r, token, username, email}, nil
+}
+
+func (r *Repo) Pull() error {
+	w, err := r.Worktree()
+	if err != nil {
+		return fmt.Errorf("pull create worktree: %w", err)
+	}
+	err = w.Pull(&git.PullOptions{
+		Auth: &http.BasicAuth{
+			Username: r.Username, // yes, this can be anything except an empty string
+			Password: r.Token,
+		},
+		RemoteName: "origin"})
+	if err != nil {
+		if err.Error() == "already up-to-date" {
+			err = nil
+		}
+	}
+	return err
+}
+
+func (r *Repo) CommitAndPush(path []string) error {
+	w, err := r.Worktree()
+	if err != nil {
+		return fmt.Errorf("commitAndPush create worktree: %w", err)
+	}
+	for _, p := range path {
+		_, err = w.Add(p)
+		if err != nil {
+			return fmt.Errorf("commitAndPush file %s %w", p, err)
+		}
+	}
+
+	_, err = w.Commit("fsnotes bots commit", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  r.Username,
+			Email: r.Email,
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("commitAndPush commit %w", err)
+	}
+
+	err = r.Push(&git.PushOptions{
+		Auth: &http.BasicAuth{
+			Username: r.Username,
+			Password: r.Token,
+		},
+	})
+
+	if err != nil {
+		return fmt.Errorf("commitAndPush push %w", err)
+	}
+
+	return nil
+}
