859ce73daf1e912ce898f12282f0b3b96ff52bb8
amamyrin <amamyrin@ozon.ru>
2025-05-02 19:48:03 +0300

    try notes

--- mcore/internal/taskmng/begindialog.go
+++ mcore/internal/taskmng/begindialog.go
@@ -16,6 +16,14 @@ next words:
 - "del #" - where # it is id torrent
 `
 
+const tnHelpText = `This is help for /note commands:
+- /note diary entry - add message to diary. Synonyms: nd/Nd
+- /note diary 5bx - add 5bx line. Synonyms: n5/N5
+- /note inbox add - add text to inbox. Synonyms: ni/Ni
+- /note inbox read - read inbox. Synonyms: nir/Nir
+- /note pull - just update repo
+`
+
 const helpCommon = ` My commands:
 - /help
 - /ping
@@ -58,6 +66,8 @@ func (m *Mng) createReply(dialogId int64, userName string, userText string, file
 		return m.createYtdlTask(dialogId, userName, userText)
 	case "/torrent", "torrent", "t", "T":
 		return m.createTrTask(dialogId, userText, fileUrl)
+	case "/note", "n", "nd", "Nd", "n5", "N5", "ni", "Ni", "nir", "Nir":
+		return m.createNoteTask(dialogId, userText)
 	}
 
 	return "", fmt.Errorf("command not found")
@@ -186,4 +196,93 @@ func chooseFolderPath(label string) (string, error) {
 	}
 
 	return "", fmt.Errorf("wrong label")
+}
+
+func (m *Mng) createNoteTask(dialogId int64, text string) (string, error) {
+	words := strings.Split(text, " ")
+	if len(words) < 2 {
+		return "", fmt.Errorf("for note need command")
+	}
+	var err error
+	if words[0] != "/note" {
+		words, err = parseSimplifications(words)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	task := schema.Task{
+		DialogId: dialogId,
+		Type:     schema.TaskTypeNote,
+		Status:   schema.TaskStatusCreate,
+		TaskData: schema.TaskData{
+			Tn: schema.TaskNote{},
+		},
+	}
+	switch words[1] {
+	case "diary":
+		{
+			if !(words[2] == "entry" || words[2] == "5bx") {
+				return "", fmt.Errorf("for diary need label")
+			}
+			task.TaskData.Tn = schema.TaskNote{
+				Command: schema.TaskNoteCmdAddDiary,
+				AddText: strings.Join(words[2:], " "),
+			}
+		}
+	case "inbox":
+		{
+			switch words[2] {
+			case "add":
+				{
+					task.TaskData.Tn = schema.TaskNote{
+						Command: schema.TaskNoteCmdAddInbox,
+						AddText: strings.Join(words[3:], " "),
+					}
+				}
+			case "read":
+				{
+					task.TaskData.Tn = schema.TaskNote{
+						Command: schema.TaskNoteReadInbox,
+					}
+				}
+			default:
+				return "", fmt.Errorf("for inbox need label")
+			}
+		}
+	case "pull":
+		{
+			task.TaskData.Tn = schema.TaskNote{
+				Command: schema.TaskNoteCmdPull,
+				AddText: "",
+			}
+		}
+	case "help":
+		{
+			return tnHelpText, nil
+		}
+	default:
+		return "", fmt.Errorf("unknown command")
+	}
+
+	_, err = m.repo.AddTask(task)
+	if err != nil {
+		return "", fmt.Errorf("taskMng add task: %w", err)
+	}
+
+	return "task tr created", nil
+}
+
+func parseSimplifications(words []string) ([]string, error) {
+	switch words[0] {
+	case "nd", "Nd":
+		return append([]string{"/note", "diary", "entry"}, words[1:]...), nil
+	case "n5", "N5":
+		return append([]string{"/note", "diary", "5bx"}, words[1:]...), nil
+	case "ni", "Ni":
+		return append([]string{"/note", "inbox", "add"}, words[1:]...), nil
+	case "nir", "Nir":
+		return append([]string{"/note", "inbox", "read"}, words[1:]...), nil
+	}
+	return nil, fmt.Errorf("synonyms command not found")
 }
