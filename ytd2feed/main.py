0a31764ffb2d6d619f77c3c5dcdf645b06f2a1b4
amamyrin <amamyrin@ozon.ru>
2025-05-03 19:24:24 +0300

    try to fix ytdl

--- ytd2feed/main.py
+++ ytd2feed/main.py
@@ -29,8 +29,8 @@ def start_download(fc: app.FeedCreater,
 
 if __name__ == '__main__':
     print("start app")
-    ytdl_verion = version('yt_dlp')
-    print("yt_dlp version", ytdl_verion)
+    # ytdl_verion = version('yt_dlp')
+    # print("yt_dlp version", ytdl_verion)
     cfg = app.Conf()
     print("mcore addr: {}, mcore_secret: {}, taskType: {}, "
           .format(cfg.mcore_addr, cfg.mcore_secret,cfg.task_type))
@@ -49,8 +49,8 @@ if __name__ == '__main__':
         if d.get("id") is None:
             time.sleep(1)  # be nice to the system :)
             continue
-        if m_client.health_reported(d, ytdl_verion):
-            continue
+        # if m_client.health_reported(d, ytdl_verion):
+        #     continue
         if not m_client.check_and_report(d):
             continue
         print("start to process taskid:",str(d["id"]), "link:", d["taskData"]["ytdl"]["link"])
