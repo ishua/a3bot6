e52a3cb5c30cdaaa0877ce08ed40a8234febc01b
Aleksey IM <alekseyim@gmail.com>
2026-06-05 18:00:38 +0300

    add docker

--- xray-manual-svc/internal/app/config/loader.go
+++ xray-manual-svc/internal/app/config/loader.go
@@ -1,23 +1,25 @@
 package config
 
 import (
-	"github.com/cristalhq/aconfig"
-	"github.com/cristalhq/aconfig/aconfigyaml"
+    "github.com/cristalhq/aconfig"
+    "github.com/cristalhq/aconfig/aconfigyaml"
 )
 
+const configPath = "/conf/xray_manual_svc_config.yaml"
+
 func Load() (*Config, error) {
-	var cfg Config
+    var cfg Config
 
-	loader := aconfig.LoaderFor(&cfg, aconfig.Config{
-		Files: []string{"config.yaml"},
-		FileDecoders: map[string]aconfig.FileDecoder{
-			".yaml": aconfigyaml.New(),
-		},
-	})
+    loader := aconfig.LoaderFor(&cfg, aconfig.Config{
+        Files: []string{configPath},
+        FileDecoders: map[string]aconfig.FileDecoder{
+            ".yaml": aconfigyaml.New(),
+        },
+    })
 
-	if err := loader.Load(); err != nil {
-		return nil, err
-	}
+    if err := loader.Load(); err != nil {
+        return nil, err
+    }
 
-	return &cfg, nil
+    return &cfg, nil
 }
